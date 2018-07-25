c4e2099b4dce9bfd7210b264446dbfb4e9b48b3f
93ctygit config --global user.email  pigpig93cty@gmail.com <pigpig93cty@gmail.com>
2018-07-25 13:56:11 +0800

    data

--- project/010-awww/src/main.js
+++ project/010-awww/src/main.js
@@ -2,20 +2,38 @@ import './css/global.css';
 
 import Vue from 'vue';
 import Router from 'vue-router';
+import Validator from 'vee-validate';
 
 import App from './App.vue';
 import Home from './page/Home';
 import Detail from './page/Detail';
 import Search from './page/Search';
 
+import Admin from './page/admin/Admin';
+import Category from './page/admin/Category';
+
 Vue.config.productionTip = false;
 Vue.use(Router);
+Vue.use(Validator, {
+  locale: 'zh_CN'
+});
 
 const router = new Router({
   routes: [
     { path: '/', component: Home },
     { path: '/detail', component: Detail },
     { path: '/search', component: Search },
+
+    {
+      path: '/admin',
+      component: Admin,
+      children: [
+        {
+          path: '/category',
+          component: Category,
+        }
+      ]
+    }
   ],
 });
 
